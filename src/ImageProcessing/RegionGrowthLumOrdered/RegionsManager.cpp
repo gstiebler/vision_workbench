8d1347baefc23708e9fbb4c04ce0a80a5ded6c77
Guilherme Stiebler <guilhermemst@gmail.com>
2016-11-01 09:50:57 -0200

    code formatting

--- src/ImageProcessing/RegionGrowthLumOrdered/RegionsManager.cpp
+++ src/ImageProcessing/RegionGrowthLumOrdered/RegionsManager.cpp
@@ -13,86 +13,73 @@ using namespace cv;
 
 #define INVALID_REGION 0
 
-RegionsManager::RegionsManager( int width, int height )
-{
-	_regionOfPixel.resize( height );
-	for(auto &ropLine : _regionOfPixel)
-	{
+RegionsManager::RegionsManager(int width, int height) {
+	_regionOfPixel.resize(height);
+	for (auto &ropLine : _regionOfPixel) {
 		ropLine.resize(width);
 		std::fill(ropLine.begin(), ropLine.end(), 0);
 	}
 
 	// null region
-	regionsIndexMap.push_back( 0 );
-	regions.push_back( new Region( this, INVALID_REGION ) );
+	regionsIndexMap.push_back(0);
+	regions.push_back(new Region(this, INVALID_REGION));
 }
 
-RegionsManager::~RegionsManager()
-{
-	for(auto &region : regions)
-	{
+RegionsManager::~RegionsManager() {
+	for (auto &region : regions) {
 		delete region;
 	}
 }
 
-Region* RegionsManager::getRegion( const Point &point )
-{
+Region* RegionsManager::getRegion(const Point &point) {
 	int originalId = _regionOfPixel[point.y][point.x];
-	if(originalId == INVALID_REGION)
-	{
+	if (originalId == INVALID_REGION) {
 		return nullptr;
 	}
 	int newId = regionsIndexMap[originalId];
 	return regions[newId];
 }
 
-void RegionsManager::setRegion( const Point &point, Region *region )
-{
+void RegionsManager::setRegion(const Point &point, Region *region) {
 	_regionOfPixel[point.y][point.x] = region->id;
 }
 
-void RegionsManager::createRegion( const Point &point )
-{
-	regionsIndexMap.push_back( regions.size() );
-	Region *region = new Region( this, regions.size() );
+void RegionsManager::createRegion(const Point &point) {
+	regionsIndexMap.push_back(regions.size());
+	Region *region = new Region(this, regions.size());
 	region->addPoint(point);
-	regions.push_back( region );
+	regions.push_back(region);
 	activeRegions[region->id] = region;
 }
 
-void RegionsManager::mergeRegions( Region *region1, Region *region2, const Point &point )
-{
-	if( region1 == region2 )
+void RegionsManager::mergeRegions(Region *region1, Region *region2,
+		const Point &point) {
+	if (region1 == region2)
 		return;
 
-	if(region1->stopped || region2->stopped)
+	if (region1->stopped || region2->stopped)
 		return;
 
-	region1->merge( region2 );
-	region2->addPoint( point );
+	region1->merge(region2);
+	region2->addPoint(point);
 
 	activeRegions.erase(region1->id);
 }
 
-void RegionsManager::processRegionsAfterLum()
-{
+void RegionsManager::processRegionsAfterLum() {
 	auto regionIt = activeRegions.begin();
-	while(regionIt != activeRegions.end())
-	{
+	while (regionIt != activeRegions.end()) {
 		Region *region = (*regionIt).second;
 		region->rectHistory.push_back(region->limits);
 
-		if(shouldStopRegionFn(*region))
-		{
+		if (shouldStopRegionFn(*region)) {
 			region->stopped = true;
 			stoppedRegions.push_back(region);
 
 			auto previous = regionIt;
 			regionIt++;
 			activeRegions.erase(previous);
-		}
-		else
-		{
+		} else {
 			regionIt++;
 		}
 	}
